6b29c19afd34230c706e1237df63e64e4ae6bef7
dkvtieu <dkhac.tieu@gmail.com>
2018-04-02 02:02:00 +0200

    Initial commit

---
+++ Library/Collab/Base/Assets/DialogueManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour {
+
+	public Text dialogueText;
+	public Text nameText;
+
+	private Queue<string> sentences;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Started Dialogue");
+
+		sentences = new Queue<string>();
+	}
+
+	public void DisplayDialogue(Dialogue dialogue) {
+		// TODO method needs to show full sentence once before skipping to next 
+		sentences.Clear();
+
+		foreach(string sentence in dialogue.sentences) {
+			sentences.Enqueue(sentence);
+		}
+		nameText.text = dialogue.name;
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence() {
+		if (sentences.Count == 0) {
+			EndDialog ();
+			return;
+		}
+		dialogueText.text = sentences.Dequeue ();
+	}
+
+
+	public void EndDialog() {
+		Debug.Log ("Ended Dialogue");
+		return;
+	}
+
+//	IEnumerator
+}
